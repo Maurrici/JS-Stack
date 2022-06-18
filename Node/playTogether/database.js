1b35da4e2deb00c6c01c6715a462da58363a3a7a
Maurrici <mauriciosantos@great.ufc.br>
2022-06-18 11:03:00 -0300

    Finish app

---
+++ Node/playTogether/database.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: "League of Legends",
+        maxPlayers: 5
+    },
+    {
+        id: 2,
+        name: "FIFA",
+        maxPlayers: 2
+    },
+    {
+        id: 3,
+        name: "Minecraft",
+        maxPlayers: 20
+    },
+    {
+        id: 4,
+        name: "Overcooked",
+        maxPlayers: 4
+    },
+    {
+        id: 5,
+        name: "Fall Guys",
+        maxPlayers: 4
+    },
+    {
+        id: 6,
+        name: "Gartic",
+        maxPlayers: 10
+    }
+];
