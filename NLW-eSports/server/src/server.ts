0720059c3cc09c55ae7bb481f4132dbafee29c24
mauriciosantos <mauriciosantos@great.ufc.br>
2022-09-13 12:57:48 -0300

    NLW-eSports dia 01

---
+++ NLW-eSports/server/src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (req, res) => {
+    return res.json([
+        {
+            id: 1,
+            name: "Anúncio 1"
+        },
+        {
+            id: 2,
+            name: "Anúncio 2"
+        },
+        {
+            id: 3,
+            name: "Anúncio 3"
+        },
+        {
+            id: 4,
+            name: "Anúncio 4"
+        },
+    ]);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running...");
+});
